c6e7b6bbaec280993ea47c5212da408047027054
fsc02939 <floriane.Scheers@open-groupe.com>
2023-01-12 12:29:13 +0100

    ajout version

--- WpfApplication/ViewModels/VersionModifications.cs
+++ WpfApplication/ViewModels/VersionModifications.cs
@@ -33,6 +33,10 @@ namespace MaCompta.ViewModels
         /// </summary>
         public static void FillVersions(List<VersionModifications> versions)
         {
+            var v11 = new VersionModifications("1.0.0.11");
+            v11.Modifications.Add("Synchronisation des opérations liées");
+            versions.Add(v11);
+
             var v10 = new VersionModifications("1.0.0.10");
             v10.Modifications.Add("Correction dispatcher");
             versions.Add(v10);
